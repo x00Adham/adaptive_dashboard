592b33a620fc643f84100757ac268c82f1421ac4
“Adham <adham.m.abdulah@gmail.com>
2025-07-13 10:44:41 +0300

    first_commit

---
+++ android/app/src/main/kotlin/com/example/adaptive_dashboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.adaptive_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:adaptive_dashboard/view/dashboard_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AdaptiveDashboardApp());
+}
+
+class AdaptiveDashboardApp extends StatelessWidget {
+  const AdaptiveDashboardApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DashboardView(),
+    );
+  }
+}
