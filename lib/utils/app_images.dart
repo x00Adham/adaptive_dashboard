ede505389f9c30bf783ded22ce5859bd21e7b82c
“Adham <adham.m.abdulah@gmail.com>
2025-07-13 21:57:39 +0300

    add image assets and generate class for assets

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAvatar1
+  /// lib/assets/images/avatar_1.svg
+  static const String assetsImagesAvatar1 = "lib/assets/images/avatar_1.svg";
+
+  /// Assets for assetsImagesAvatar2
+  /// lib/assets/images/avatar_2.svg
+  static const String assetsImagesAvatar2 = "lib/assets/images/avatar_2.svg";
+
+  /// Assets for assetsImagesAvatar3
+  /// lib/assets/images/avatar_3.svg
+  static const String assetsImagesAvatar3 = "lib/assets/images/avatar_3.svg";
+
+  /// Assets for assetsImagesBalance
+  /// lib/assets/images/balance.svg
+  static const String assetsImagesBalance = "lib/assets/images/balance.svg";
+
+  /// Assets for assetsImagesCardBackground
+  /// lib/assets/images/card_background.png
+  static const String assetsImagesCardBackground = "lib/assets/images/card_background.png";
+
+  /// Assets for assetsImagesDashboard
+  /// lib/assets/images/dashboard.svg
+  static const String assetsImagesDashboard = "lib/assets/images/dashboard.svg";
+
+  /// Assets for assetsImagesExpenses
+  /// lib/assets/images/expenses.svg
+  static const String assetsImagesExpenses = "lib/assets/images/expenses.svg";
+
+  /// Assets for assetsImagesGallery
+  /// lib/assets/images/gallery.svg
+  static const String assetsImagesGallery = "lib/assets/images/gallery.svg";
+
+  /// Assets for assetsImagesIncome
+  /// lib/assets/images/income.svg
+  static const String assetsImagesIncome = "lib/assets/images/income.svg";
+
+  /// Assets for assetsImagesLogout
+  /// lib/assets/images/logout.svg
+  static const String assetsImagesLogout = "lib/assets/images/logout.svg";
+
+  /// Assets for assetsImagesMyInvestments
+  /// lib/assets/images/my_investments.svg
+  static const String assetsImagesMyInvestments = "lib/assets/images/my_investments.svg";
+
+  /// Assets for assetsImagesMyTransctions
+  /// lib/assets/images/my_transctions.svg
+  static const String assetsImagesMyTransctions = "lib/assets/images/my_transctions.svg";
+
+  /// Assets for assetsImagesSettings
+  /// lib/assets/images/settings.svg
+  static const String assetsImagesSettings = "lib/assets/images/settings.svg";
+
+  /// Assets for assetsImagesStatistics
+  /// lib/assets/images/statistics.svg
+  static const String assetsImagesStatistics = "lib/assets/images/statistics.svg";
+
+  /// Assets for assetsImagesWalletAccount
+  /// lib/assets/images/wallet_account.svg
+  static const String assetsImagesWalletAccount = "lib/assets/images/wallet_account.svg";
+}
+
