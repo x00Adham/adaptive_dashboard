6852203efaf658c9d310d2f59f69c7cf21231788
“Adham <adham.m.abdulah@gmail.com>
2025-07-13 11:02:26 +0300

    Adaptive Layout done

---
+++ lib/widget/adaptive_layout_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayoutWidget extends StatelessWidget {
+  const AdaptiveLayoutWidget({
+    super.key,
+    required this.mobileLayout,
+    required this.tabletLayout,
+    required this.desktopLayout,
+  });
+  final WidgetBuilder mobileLayout;
+  final WidgetBuilder tabletLayout;
+  final WidgetBuilder desktopLayout;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return mobileLayout(context);
+        } else if (constraints.maxWidth < 900) {
+          return tabletLayout(context);
+        } else {
+          return desktopLayout(context);
+        }
+      },
+    );
+  }
+}
